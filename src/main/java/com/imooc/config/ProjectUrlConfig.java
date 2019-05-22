849aa14652de622bc2a7e632cc1d873431aebadd
githubly <2127186681@qq.com>
2019-05-22 17:47:57 +0800

    一次修改

--- src/main/java/com/imooc/config/ProjectUrlConfig.java
+++ src/main/java/com/imooc/config/ProjectUrlConfig.java
@@ -3,7 +3,7 @@ package com.imooc.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-//
+
 @Data
 @ConfigurationProperties(prefix = "projectUrl")
 @Component
